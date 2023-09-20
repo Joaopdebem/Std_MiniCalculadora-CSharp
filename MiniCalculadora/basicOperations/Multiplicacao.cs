1bf08c99f9752064c1811d47cc2942bfd8b9c220
João Pedro de Bem <jpdebem@gmail.com>
2023-09-20 15:31:44 -0300

    Subindo arquivos

---
+++ MiniCalculadora/basicOperations/Multiplicacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniCalculadora.basicOperations
+{
+    public class Multiplicacao
+    {
+        public double Calcular(double multiplicando, double multiplicador)
+        {
+            return multiplicando * multiplicador;
+        }
+
+    }
+}
